9ec2fc7f5b875dddd04b03f1b3da037891291428
Abdall_EM <abdallaemad1.3.2.0.0.5@gmail.com>
2024-12-05 15:59:52 +0200

    wisth the latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-
 import HeroSection from "@/components/landing/hero-secontion";
 import AboutSection from '@/components/landing/about-section'
 import ProjectsGallery from "@/components/landing/prject gallery";
 export default function Home() {
 
   return (
-    <main className="min-h-[300dvh]">
+    <main>
       <HeroSection />
       <AboutSection />
       <ProjectsGallery />
